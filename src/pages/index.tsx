1a395ee626e14f3a8167167e64536d26b44647d0
Ruairidh Grass <ruripg@gmail.com>
2023-02-14 20:52:55 +0100

    start vocab game button added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,13 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
 import Link from 'next/link'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
+  const [showVocabGame, setShowVocabGame] = useState<Boolean>(false)
+
+
   return (
     <>
       <Head>
@@ -14,10 +17,20 @@ const Home = () => {
       </Head>
       <Header />
       <div className=" h-screen min-h-screen -mt-14 flex justify-center items-center bg-duo-eel">
-        <div className='flex flex-col bg-blue-600 rounded-xl mx-2 w-full sm:w-4/5 md:w-3/4 lg:w-1/2 py-9 px-3 md:px-9 bg-duo-hare border-b-4 border-b-duo-wolf'>
-          <button className='button bg-duo-macaw'>
-            <Link href="/vocabulary">Vocabulary Game </Link>
+        <div className='flex flex-col space-y-2 bg-blue-600 rounded-xl mx-2 w-full sm:w-4/5 md:w-3/4 lg:w-1/2 py-9 px-3 md:px-9 bg-duo-hare border-b-4 border-b-duo-wolf'>
+          <button className='button bg-duo-macaw' onClick={() => setShowVocabGame(!showVocabGame)}>
+            Vocabulary
           </button>
+          {showVocabGame &&
+            <div className='flex justify-center space-y-2 py-2 bg-duo-wolf rounded-xl border-t-4 border-b-duo-eel'>
+              <button className='button bg-duo-macaw'>
+                <Link href="/vocabulary">Start Game </Link>
+              </button>
+            </div>
+          }
+          {/* <button className='button bg-duo-macaw' >
+            Genders
+          </button> */}
         </div>
       </div>
     </>
