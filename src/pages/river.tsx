cde61538d7903ccf32b17340dda252c12176cff8
rurigrass <ruripg@gmail.com>
2023-05-02 16:53:20 +0200

    river menu extended

--- src/pages/river.tsx
+++ src/pages/river.tsx
@@ -21,7 +21,7 @@
 
 
 const River = () => {
-    const levels: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const levels: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
     return (
         <div className="flex flex-col items-center">
