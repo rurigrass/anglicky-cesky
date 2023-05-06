a49fef25b7b468e074f8f87f861e867a489795c4
rurigrass <ruripg@gmail.com>
2023-05-06 23:18:45 +0200

    header logo bold

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <header className="flex justify-between px-5 py-2 h-14 bg-duo-gray sticky border-b-black">
-            <Link href={"/"} className="flex items-center space-x-5 font-extrabold">
+            <Link href={"/"} className="flex items-center space-x-5 font-bold">
                 Anglicky - Cesky
             </Link>
         </header >
